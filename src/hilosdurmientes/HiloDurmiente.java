34d36683635bd48d149c4d779e0742dbb8b57d3a
josem.ferreira@iesnervion.es <josem.ferreira@iesnervion.es>
2022-11-15 14:44:29 +0100

    hilo durmiente

--- src/hilosdurmientes/HiloDurmiente.java
+++ src/hilosdurmientes/HiloDurmiente.java
@@ -18,14 +18,24 @@ public class HiloDurmiente extends Thread {
         while (true){
             Thread hilo = currentThread();
             System.out.println("Soy el bucle"+hilo.getName()+"y estoy trabajando");
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     public static void main(String[] args) {
         HiloDurmiente hilo1 = new HiloDurmiente("Hilo 1");
+        hilo1.start();
         HiloDurmiente hilo2 = new HiloDurmiente("Hilo 2");
+        hilo2.start();
         HiloDurmiente hilo3 = new HiloDurmiente("Hilo 3");
+        hilo3.start();
         HiloDurmiente hilo4 = new HiloDurmiente("Hilo 4");
+        hilo4.start();
         HiloDurmiente hilo5 = new HiloDurmiente("Hilo 5");
+        hilo5.start();
     }
 }
