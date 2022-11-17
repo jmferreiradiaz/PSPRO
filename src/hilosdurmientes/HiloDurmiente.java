791df284be4d589376fdf863eefa52c9061027b5
josem.ferreira@iesnervion.es <josem.ferreira@iesnervion.es>
2022-11-17 12:49:55 +0100

    hilo número oculto

--- src/hilosdurmientes/HiloDurmiente.java
+++ src/hilosdurmientes/HiloDurmiente.java
@@ -8,12 +8,16 @@ public class HiloDurmiente extends Thread {
 
     @Override
     public void run(){
-        int numAle = (int)Math.floor(Math.random()*10+1);
+        //Generamos un int Aleatorio entre 1 y 10 incluidos
+        int numAle = (int)(Math.random()*10)+1;
+
         while (true){
+            //Cogemos el hilo que está actualmente en ejecucíón
             Thread hilo = currentThread();
             System.out.println("Soy el bucle "+hilo.getName()+" y estoy trabajando");
             try {
-                Thread.sleep(100 * numAle);
+                //Detenemos la ejecución entre 1 y 10 segundos
+                Thread.sleep(1000 * numAle);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -21,20 +25,25 @@ public class HiloDurmiente extends Thread {
     }
 
     public static void main(String[] args) {
+        //Declaramos e inicializamos los hilos
         HiloDurmiente hilo1 = new HiloDurmiente();
-        hilo1.setName("hilo1");
-        hilo1.start();
         HiloDurmiente hilo2 = new HiloDurmiente();
-        hilo2.setName("hilo2");
-        hilo2.start();
         HiloDurmiente hilo3 = new HiloDurmiente();
-        hilo3.setName("hilo3");
-        hilo3.start();
         HiloDurmiente hilo4 = new HiloDurmiente();
-        hilo4.setName("hilo4");
-        hilo4.start();
         HiloDurmiente hilo5 = new HiloDurmiente();
-        hilo5.setName("hilo5");
+
+        //Asignamos un nombre a cada hilo
+        hilo1.setName("hilo 1");
+        hilo2.setName("hilo 2");
+        hilo3.setName("hilo 3");
+        hilo4.setName("hilo 4");
+        hilo5.setName("hilo 5");
+
+        //Lanzamos los hilos
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
         hilo5.start();
     }
 }
--- src/hilosnumerooculto/HiloNumeroOculto.java
+++ src/hilosnumerooculto/HiloNumeroOculto.java
@@ -6,30 +6,39 @@ public class HiloNumeroOculto extends Thread{
 
     @Override
     public void run(){
-        int numero = (int)Math.random();
+        int numero = (int)(Math.random()*100)+1;
         int res = propuestaNumero(numero);
 
         while(res == 0){
-            numero = (int)Math.random();
+            numero = (int)(Math.random()*100)+1;
             res = propuestaNumero(numero);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+
+            }
         }
         //Interrumpir en cualquier caso
     }
 
-    public static int propuestaNumero(int numero){
+    public static synchronized int propuestaNumero(int numero){
         int res = 0;
-        if (numero == numeroAdivinar){
+        if (numero == numeroAdivinar && !acertado){
             res = 1;
+            acertado = true;
+            System.out.println("El hilo "+currentThread().getName()+" ha averiguado el número secreto "+numeroAdivinar);
             //Interrumpir hilo
+            currentThread().interrupt();
         }if(acertado) {
             res =-1;
             //Interrumpir hilo
+            currentThread().interrupt();
         }
         return res;
     }
 
     public static void main(String[] args) {
-        numeroAdivinar = (int)Math.random();
+        numeroAdivinar = (int)(Math.random()*100)+1;
 
         for (int i = 1; i<=10 ; i++){
             new HiloNumeroOculto().start();
